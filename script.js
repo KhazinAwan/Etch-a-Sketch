3a2efb0e5b25d726fe4a3aa4e981c70d91ffef53
Khazin <175429319+KhazinAwan@users.noreply.github.com>
2026-03-20 17:34:33 +0500

    Change color to user input color upon hover
    Added a color picker to take user input color
    Changing color upon hover
    Added a Clear Grid button to clear grid of colors

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-const sqaureGridSize = 16
+const squareGridSize = 16
+const color = document.querySelector("#colorPicker")
 
 const grid = document.querySelector(".grid")
+const clearGridButton = document.querySelector(".btn")
 
-for(let i = 0 ; i < (sqaureGridSize * sqaureGridSize); i++) {
+clearGridButton.addEventListener("click" , () => {
+
+    const squares = document.querySelectorAll(".square")
+
+    for(let i = 0 ; i < (squareGridSize * squareGridSize); i++) {
+
+        squares.forEach((square) => square.style.backgroundColor = "white")
+
+    }
+        
+})
+
+for(let i = 0 ; i < (squareGridSize * squareGridSize); i++) {
 
     const square = document.createElement("div")
     square.classList.add("square")
 
     grid.appendChild(square)
 
+    square.addEventListener("mouseenter" , () => {
+
+        square.style.backgroundColor = color.value
+    })
+
 }
+
+
+
+
 
 
 
